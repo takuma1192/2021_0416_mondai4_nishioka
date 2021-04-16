293d0f179b90ed5d429049679e78aeb2f8e7434d
takuma1192 <78262525+takuma1192@users.noreply.github.com>
2021-04-16 17:58:51 +0900

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul>
+      <li>
+        <router-link :to="{ name: 'Member' , params: { id: 1 }}" >
+          1へ進む
+        </router-link>  
+      </li>
+      <li>
+        <router-link :to="{ name: 'Member' , params: { id: 2 }}" >
+          2へ進む
+        </router-link>  
+      </li>
+      <li>
+        <router-link :to="{ name: 'Member' , params: { id: 3 }}" >
+          3へ進む
+        </router-link>  
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+ul {
+  padding: opx;
+}
+li {
+  list-style: none;
+}
+</style>
